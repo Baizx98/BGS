823d8178469540b4b28e04335d2e71354be160c4
Baizx98 <baizx98@gmail.com>
2023-11-27 11:07:01 +0800

    Add logging and create a benchmark for Yelp dataset

--- benchmarks/bench_dataset.py
+++ benchmarks/bench_dataset.py
@@ -1,4 +1,5 @@
 from collections import deque
+import logging
 import torch as th
 import numpy as np
 import torch_geometric as pyg
@@ -6,6 +7,10 @@ from ogb.nodeproppred import PygNodePropPredDataset
 import torch_geometric.datasets as pyg_dataset
 
 from bgs.graph import CSRGraph
+
+from utils import DatasetCreator
+
+root = "/home8t/bzx/data/"
 
 
 def bench_on_ogbn_products():
@@ -59,6 +64,17 @@ def bench_on_reddit():
     print("is undirected:", data.is_undirected())
 
 
+def bench_on_yelp():
+    dataset = pyg_dataset.Yelp(root="/home8t/bzx/data/yelp")
+    data = dataset[0]
+    train_ids = data.train_mask.nonzero(as_tuple=False).view(-1)
+    print(train_ids.shape)
+    print("node nums:", data.num_nodes)
+    print("edge nums:", data.num_edges)
+    print("is undirected:", data.is_undirected())
+    print("train node nums:", train_ids.shape)
+
+
 def bench_on_pyg_undirected_data():
     edge_index = th.tensor([[0, 1, 1, 2], [1, 0, 2, 3]])
     data = pyg.data.Data(edge_index=edge_index, num_nodes=4)
@@ -67,11 +83,55 @@ def bench_on_pyg_undirected_data():
 
 
 def bench_connected_graph_nums(dataset_name: str):
-    pass
+    logger.info(f"Start benching {dataset_name} dataset")
+    data, csr_graph, train_ids = DatasetCreator.pyg_dataset_creator(
+        dataset_name, root=root
+    )
+
+    connected_graphs = []
+    visited = set()
+
+    for node in train_ids:
+        if node not in visited:
+            connected_graph = set()
+            stack = [node]
+
+            while stack:
+                current_node = stack.pop()
+                if current_node not in visited:
+                    visited.add(current_node)
+                    connected_graph.add(current_node)
+
+                    neighbors = csr_graph.indices[
+                        csr_graph.indptr[current_node] : csr_graph.indptr[
+                            current_node + 1
+                        ]
+                    ]
+                    stack.extend(neighbors)
+
+            connected_graphs.append(connected_graph)
+
+    num_connected_graphs = len(connected_graphs)
+    logger.info(f"Number of connected graphs: {num_connected_graphs}")
+    return num_connected_graphs
 
 
 if __name__ == "__main__":
+    # logging setup
+    logger = logging.getLogger("dataset-bench")
+    logger.setLevel(logging.INFO)
+    file_handler = logging.FileHandler("log/bench_dataset.log", mode="a")
+    file_handler.setLevel(logging.INFO)
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.INFO)
+    formatter = logging.Formatter("%(asctime)s %(filename)s %(levelname)s %(message)s")
+    file_handler.setFormatter(formatter)
+    console_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
+
     # bench_on_ogbn_products()
     # bench_on_livejournal()
     # bench_on_pyg_undirected_data()
-    bench_on_reddit()
+    # bench_on_reddit()
+    bench_on_yelp()
