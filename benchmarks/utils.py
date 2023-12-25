9e58cbfcedb023f4cff75acd7a8bc9da57691ac0
Baizx98 <baizx98@gmail.com>
2023-12-25 10:13:48 +0800

    Add linear_greedy_placement method and refactor

--- benchmarks/utils.py
+++ benchmarks/utils.py
@@ -1,6 +1,7 @@
 import os
 import logging
 from abc import ABC, abstractmethod
+from torch._tensor import Tensor
 from tqdm import tqdm
 
 import torch as th
@@ -119,7 +120,13 @@ class DataPlacement(ABC):
     ) -> list[th.Tensor]:
         """生成每个GPU上要缓存的部分"""
         pass
+        # 在子类重写的方法中，调用数据放置方法的具体实现，这样可以在基类实现扩展，在子类中灵活使用
         return
+
+    def linear_greedy_placement(self, **kwargs):
+        """数据放置方法的具体实现"""
+        # TODO 实现
+        pass
 
 
 class CachePagraph(Cache):
@@ -159,16 +166,13 @@ class CacheGnnlab(Cache):
     def generate_probability(self, **kwargs) -> th.Tensor:
         csr_graph: CSRGraph = kwargs.get("csr_graph")
         loader_list: list[NeighborLoader] = kwargs.get("loader_list")
-        gpu_list: list[int] = kwargs.get("gpu_list")
         pre_sampler_epoches: int = kwargs.get("pre_sampler_epoches", 3)
-        # 为了支持全局cache和分区cache
-        assert len(loader_list) == len(gpu_list)
 
-        freq: th.Tensor = th.zeros(csr_graph.node_count, dtype=int)
-        for i in range(self.world_size):
-            for j in range(pre_sampler_epoches):
+        freq: th.Tensor = th.zeros(csr_graph.node_count, dtype=float)
+        for _ in range(pre_sampler_epoches):
+            for loader in loader_list:
                 ### 解决LOADER传递的问题，直接传递
-                for minibatch in loader_list[i]:
+                for minibatch in loader:
                     freq[minibatch.n_id] += 1
         probability = F.normalize(freq, p=1, dim=0)
         return probability
@@ -190,7 +194,6 @@ class CacheGnnlab(Cache):
         probability = self.generate_probability(
             csr_graph=csr_graph,
             loader_list=loader_list,
-            gpu_list=[i for i in range(self.world_size)],
             pre_sampler_epoches=pre_sampler_epoches,
         )
         sorted_nid = th.argsort(probability, descending=True)
@@ -215,6 +218,10 @@ class CacheGnnlabPartition(CacheGnnlab, DataPlacement):
 
     # generate_probability方法使用父类CacheGnnlab的方法
 
+    # generate_layout
+    def generate_layout(self, access_probability_list: list[Tensor]) -> list[Tensor]:
+        return super().generate_layout(access_probability_list)
+
     def generate_cache(self, **kwargs):
         """
         Generate cache for each GPU.
@@ -232,8 +239,7 @@ class CacheGnnlabPartition(CacheGnnlab, DataPlacement):
         probability_list = [
             self.generate_probability(
                 csr_graph=csr_graph,
-                loader_list=loader_list[i],
-                gpu_list=[i],
+                loader_list=loader_list[i : i + 1],
                 pre_sampler_epoches=pre_sampler_epoches,
             )
             for i in range(self.world_size)
@@ -241,7 +247,7 @@ class CacheGnnlabPartition(CacheGnnlab, DataPlacement):
         # 对freq_list中的每个tensor进行降序排序，返回排序后的元素的索引
         sorted_nid_list = []
         for i in range(self.world_size):
-            sorted_nid_list[i] = th.argsort(probability_list[i], descending=True)
+            sorted_nid_list.append(th.argsort(probability_list[i], descending=True))
         # 设置每个GPU缓存节点的数量
         for gpu_id in range(self.world_size):
             self.cache_nodes_to_gpu(
@@ -324,7 +330,9 @@ class CacheMutilMetric(Cache):
         device: th.device = kwargs.get("device", th.device("cpu"))
 
         cache_size = int(self.cache_ratio * csr_graph.node_count)
-        probability = self.generate_probability(csr_graph, train_ids, device)
+        probability = self.generate_probability(
+            csr_graph=csr_graph, train_ids=train_ids, device=device
+        )
         sorted_nid = th.argsort(probability, descending=True)
         for gpu_nid in range(self.world_size):
             self.cache_nodes_to_gpu(gpu_nid, sorted_nid[:cache_size].tolist())
@@ -339,10 +347,14 @@ class CacheMutilMetricPartition(CacheMutilMetric, DataPlacement):
 
     # generate_probability方法使用父类CacheMutilMetric的方法
 
+    # generate_layout
+    def generate_layout(self, access_probability_list: list[Tensor]) -> list[Tensor]:
+        return super().generate_layout(access_probability_list)
+
     def generate_cache(self, **kwargs):
         csr_graph: CSRGraph = kwargs.get("csr_graph")
-        part_dict: dict[int, list[int]] = kwargs.get("part_dict")
-        device: th.device = kwargs.get("device", th.device("cpu"))
+        train_ids_list: list[th.Tensor] = kwargs.get("train_ids_list")
+        device_list: th.device = kwargs.get("device", th.device("cpu"))
         # TODO part_dict转为tensor的list
         # TODO gpu list需要处理，最终用多个GPU来完成
 
@@ -350,13 +362,15 @@ class CacheMutilMetricPartition(CacheMutilMetric, DataPlacement):
         # TODO 需要DDP处理
         probability_list = [
             self.generate_probability(
-                csr_graph=csr_graph, train_ids=part_dict[i], device=device
+                csr_graph=csr_graph,
+                train_ids=train_ids_list[i].tolist(),
+                device=device_list[i % len(device_list)],
             )
             for i in range(self.world_size)
         ]
         sorted_nid_list = []
         for i in range(self.world_size):
-            sorted_nid_list[i] = th.argsort(probability_list[i], descending=True)
+            sorted_nid_list.append(th.argsort(probability_list[i], descending=True))
         # 设置每个GPU缓存节点的数量
         for gpu_id in range(self.world_size):
             self.cache_nodes_to_gpu(
